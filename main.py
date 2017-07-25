5dd8fc0e9c8181577af0961bcb94fba42ca031d4
mk <cody.long.cl21@gmail.com>
2017-07-24 18:56:10 -0700

    update

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'f8wv3w2f>v9j4sEuhcNYydAGMzzZJgkGgyHE9gUqaJcCk^f*^o7fQyBT%XtTvcYM'
 
-'''
+
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
@@ -20,14 +20,14 @@ class User(db.Model):
     def __init__(self, email, password):
         self.email = email
         self.password = password
-        
+
 
 @app.before_request
 def require_login():
     allowed_routes = ['login', 'register']
     if request.endpoint not in allowed_routes and 'email' not in session:
         return redirect('/login')
-'''
+
 
 class Entry(db.Model):
    
@@ -37,7 +37,7 @@ class Entry(db.Model):
     created = db.Column(db.DateTime)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, body ):
+    def __init__(self, title, body, user ):
         self.title = title
         self.body = body
         self.created = datetime.utcnow()
